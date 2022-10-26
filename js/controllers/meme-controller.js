444447c8b5f8e6c18f5ecf792806dee517cdb17d
Shilo <shilo.klein1@gmail.com>
2022-10-27 01:41:27 +0300

    drag and drop and some mouse events featurs added

--- js/controllers/meme-controller.js
+++ js/controllers/meme-controller.js
@@ -3,7 +3,7 @@
 let gCanvas
 let gCtx
 
-// let cuur
+let gStartPos
 
 const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
 
@@ -12,26 +12,38 @@ function initCanvas() {
     gCanvas = document.querySelector('canvas')
     gCtx = gCanvas.getContext('2d')
     initEventListeners()
-    // resizeCanvas()
+    resizeCanvas()
 }
 // EVENT LISTENERS////////////
 function initEventListeners() {
-    gCanvas.addEventListener('mousedown', canvasClicked)
+    window.addEventListener('resize', resizeCanvas)
+    addMouseListeners()
+    addTouchListeners()
+}
+
+function addMouseListeners() {
+    gCanvas.addEventListener('mousemove', onMove)
+    gCanvas.addEventListener('mousedown', onDown)
+    gCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gCanvas.addEventListener('touchmove', onMove)
+    gCanvas.addEventListener('touchstart', onDown)
+    gCanvas.addEventListener('touchend', onUp)
 }
 
 
 
+// function onDown(ev) {
+//     console.log(ev);
+//     const clickedText = gMeme.lines.find(line => {
+//         // Check if the click coordinates are inside the bar coordinates
 
-
-function canvasClicked(ev) {
-    console.log(ev);
-    const clickedText = gMeme.lines.find(line => {
-        // Check if the click coordinates are inside the bar coordinates
-
-        return ev.offsetX > star.x && ev.offsetX < star.x + BAR_WIDTH &&
-            ev.offsetY > star.y && ev.offsetY < star.y + star.rate
-    })
-}
+//         return ev.offsetX > star.x && ev.offsetX < star.x + BAR_WIDTH &&
+//             ev.offsetY > star.y && ev.offsetY < star.y + star.rate
+//     })
+// }
 
 
 
@@ -47,6 +59,7 @@ function drawImg(img) {
     // Draw the img on the canvas
     gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
     renderText()
+    setTextBorder()
 
 }
 function renderText() {
@@ -54,22 +67,20 @@ function renderText() {
     gMeme.lines.forEach((line) => {
         const canvasWidth = gCanvas.width
         const canvasHeight = gCanvas.height
-        const { txt, size, align, color } = line
+        const { txt, size, align, strokeColor, fillColor } = line
         gCtx.lineWidth = 2
-        gCtx.strokeStyle = color
-        gCtx.fillStyle = 'black'
+        gCtx.strokeStyle = strokeColor
+        gCtx.fillStyle = fillColor
         gCtx.textAlign = align
-
+        const isDrag = getDragStatus()
         // font height; 
-        setLinePos(idx, canvasWidth, canvasHeight)
-        console.log(line);
+        if(!isDrag)setLinePos(idx, canvasWidth, canvasHeight)
 
         gCtx.font = `${size}px Arial`
 
         gCtx.fillText(txt, line.x, line.y) // Draws (fills) a given text at the given (x, y) position.
         gCtx.strokeText(txt, line.x, line.y) // Draws (strokes) a given text at the given (x, y) position.
         line.width = gCtx.measureText(txt).width
-        console.log(gMeme.lines);
         idx++
     })
 
@@ -79,11 +90,15 @@ function resizeCanvas() {
 
     const elContainer = document.querySelector('.canvas-container')
     // Note: changing the canvas dimension this way clears the canvas
-    gCanvas.width = elContainer.offsetWidth
+    console.log(elContainer);
+    gCanvas.width = elContainer.offsetWidth - 50
+    gCanvas.height = elContainer.offsetWidth - 50
+    console.log(gCanvas.width);
+    renderMeme()
     // console.log(elContainer);
     // console.log(gCanvas.width);
     // Unless needed, better keep height fixed.
-    gCanvas.height = elContainer.offsetHeight
+    // gCanvas.height = elContainer.offsetHeight
 }
 
 
@@ -96,7 +111,12 @@ function onTextInput(txt) {
 }
 
 function onStrokeColorChange(color) {
-    setTextColor(color)
+    setTextStrokeColor(color)
+    renderMeme()
+}
+function onFillColorChange(color) {
+    console.log(color);
+    setTextFillColor(color)
     renderMeme()
 }
 function onFontIncrease() {
@@ -109,29 +129,91 @@ function onFontDecrease() {
 }
 function onSwitchLines() {
     setLineIdx()
+    renderMeme()
 }
 
-// SPECIFIC SHAPE DRAW FUNCTIONS
+// SPECIFIC SHAPE DRAW FUNCTIONSgMeme
 
-function drawText(text, x, y) {
-    const size = gMeme.lines[gMeme.selectedLineIdx].size
-    gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'brown'
-    gCtx.fillStyle = 'black'
+// function drawText(text, x, y) {
+//     const size = gMeme.lines[gMeme.selectedLineIdx].size
+//     gCtx.lineWidth = 2
+//     gCtx.strokeStyle = 'brown'
+//     gCtx.fillStyle = 'black'
 
-    gCtx.font = `2px Arial`
-    gCtx.fillText(text, x - 40, y) // Draws (fills) a given text at the given (x, y) position.
-    gCtx.strokeText(text, x - 40, y) // Draws (strokes) a given text at the given (x, y) position.
+//     gCtx.font = `2px Arial`
+//     gCtx.fillText(text, x - 40, y) // Draws (fills) a given text at the given (x, y) position.
+//     gCtx.strokeText(text, x - 40, y) // Draws (strokes) a given text at the given (x, y) position.
+// }
+
+function setTextBorder() {
+
+    // console.log(width, height, x, y, lineIdx);
+    // console.log(x-width/2, y, x+width/2, y-height);
+    gCtx.beginPath()
+    const { x, y, width, height } = getTextPosition(true)
+    gCtx.rect(x, y, width, height);
+    gCtx.stroke();
 }
+
+
 
 // HELPER FUNCTIONS
 
+
+
+// DRAG AND DROP
+
+function onDown(ev) {
+    //Get the ev pos from mouse or touch
+    const pos = getEvPos(ev)
+    const line = isTextClicked(pos)
+    if (!line) return
+    console.log(line.idx);
+    setLineIdx(line.idx)
+    setLineDrag(true)
+    renderMeme()
+    console.log(gMeme.lines[gMeme.selectedLineIdx].isDrag);
+    //Save the pos we start from 
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
+
+}
+
+function onMove(ev) {
+    const meme = getCurrMeme()
+    const { isDrag } = meme.lines[meme.selectedLineIdx]
+    if (!isDrag) return
+    const pos = getEvPos(ev)
+    console.log();
+    //Calc the delta , the diff we moved
+    const dx = pos.x - gStartPos.x
+    const dy = pos.y - gStartPos.y
+    console.log(gStartPos);
+    console.log(pos);
+    console.log(dx,dy);
+    moveText(dx, dy)
+    //Save the last pos , we remember where we`ve been and move accordingly
+    gStartPos = pos
+    renderMeme()
+    //The canvas is render again after every move
+    document.body.style.cursor = 'grab'
+
+
+}
+
+function onUp() {
+    setLineDrag(false)
+    document.body.style.cursor = 'auto'
+}
+
 function getEvPos(ev) {
+
+    //Gets the offset pos , the default pos
     let pos = {
         x: ev.offsetX,
         y: ev.offsetY
     }
-
+    // Check if its a touch ev
     if (TOUCH_EVS.includes(ev.type)) {
         //soo we will not trigger the mouse ev
         ev.preventDefault()
@@ -146,5 +228,34 @@ function getEvPos(ev) {
     return pos
 }
 
+// SERVICE
+function isTextClicked(clickedPos) {
+    console.log(clickedPos);
+    const positions = getTextPosition()
+    const clickedText = positions.find(textPos => {
+        // Check if the click coordinates are inside the bar coordinates
+        // console.log(clickedPos.x > textPos.x && clickedPos.x < textPos.x + textPos.width &&
+        //     clickedPos.y > textPos.y && clickedPos.y < textPos.y + textPos.height);
+        return clickedPos.x > textPos.x && clickedPos.x < textPos.x + textPos.width &&
+        clickedPos.y > textPos.y && clickedPos.y < textPos.y + textPos.height
+      })
+      return clickedText
+    //   console.log(clickedText);
+    // gMeme.lines
+    // const { x, y } = gMeme.lines
+   
+}
 
 
+function setLineDrag(isDrag) {
+    gMeme.lines[gMeme.selectedLineIdx].isDrag = isDrag
+    // gMeme.lines[gMeme.selectedLineIdx].isDrag
+}
+
+function moveText(dx, dy) {
+    gMeme.lines[gMeme.selectedLineIdx].x += dx
+    gMeme.lines[gMeme.selectedLineIdx].y += dy
+    // console.log(dx);
+    console.log(gMeme.lines[gMeme.selectedLineIdx].x);
+    // console.log(gMeme.lines[gMeme.selectedLineIdx].y);
+}  
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -9,6 +9,20 @@ var gMemes = [
     { id: 2, url: 'meme-imgs/2.jpg', keywords: ['funny', 'cat'], },
     { id: 3, url: 'meme-imgs/3.jpg', keywords: ['funny', 'cat'], },
     { id: 4, url: 'meme-imgs/4.jpg', keywords: ['funny', 'cat'], },
+    { id: 5, url: 'meme-imgs/5.jpg', keywords: ['funny', 'cat'], },
+    { id: 6, url: 'meme-imgs/6.jpg', keywords: ['funny', 'cat'], },
+    { id: 7, url: 'meme-imgs/7.jpg', keywords: ['funny', 'cat'], },
+    { id: 8, url: 'meme-imgs/8.jpg', keywords: ['funny', 'cat'], },
+    { id: 9, url: 'meme-imgs/9.jpg', keywords: ['funny', 'cat'], },
+    { id: 10, url: 'meme-imgs/10.jpg', keywords: ['funny', 'cat'], },
+    { id: 11, url: 'meme-imgs/11.jpg', keywords: ['funny', 'cat'], },
+    { id: 12, url: 'meme-imgs/12.jpg', keywords: ['funny', 'cat'], },
+    { id: 13, url: 'meme-imgs/13.jpg', keywords: ['funny', 'cat'], },
+    { id: 14, url: 'meme-imgs/14.jpg', keywords: ['funny', 'cat'], },
+    { id: 15, url: 'meme-imgs/15.jpg', keywords: ['funny', 'cat'], },
+    { id: 16, url: 'meme-imgs/16.jpg', keywords: ['funny', 'cat'], },
+    { id: 17, url: 'meme-imgs/17.jpg', keywords: ['funny', 'cat'], },
+    { id: 18, url: 'meme-imgs/18.jpg', keywords: ['funny', 'cat'], },
 ];
 var gMeme = {
     selectedImgId: 1,
@@ -16,21 +30,34 @@ var gMeme = {
     lines: [
         {
             txt: 'I sometimes eat Falafel',
-            size: 20,
+            size: 40,
             align: 'center',
-            color: 'red',
+            srokeColor: 'black',
+            fillColor:'white',
+            width:0,
             x: 0,
             y: 0,
+            idx:0,
+            isDrag: false,
         },
         {
             txt: 'For me puky is like jquery',
-            size: 15,
+            size: 40,
             align: 'center',
-            color: 'green',
+            strokeColor: 'black',
+            fillColor:'white',
+            width:0,
             x:0,
             y:0,
+            idx:1,
+            isDrag: false,
         },
     ]
+}
+
+function createMeme(){
+    const id = gMemes.length+1
+const meme = { id: i, url: `meme-imgs/${i}.jpg`, keywords: ['funny', 'cat'], }
 }
 
 
@@ -38,25 +65,33 @@ function setImg(imgId) {
     gMeme.selectedImgId = imgId;
 }
 
-function setTextColor(color) {
+function setTextStrokeColor(color) {
     const { selectedLineIdx } = gMeme
-    gMeme.lines[selectedLineIdx].color = color
+    gMeme.lines[selectedLineIdx].strokeColor = color
+}
+function setTextFillColor(color) {
+    const { selectedLineIdx } = gMeme
+    gMeme.lines[selectedLineIdx].fillColor = color
 }
 
 function setFontSize(size) {
     const { selectedLineIdx } = gMeme
     gMeme.lines[selectedLineIdx].size += size
 }
-function setLineIdx() {
-    gMeme.selectedLineIdx
+function setLineIdx(idx) {
+    // gMeme.selectedLineIdx
+    if(idx){
+        gMeme.selectedLineIdx=idx
+        return
+    }
     if(gMeme.selectedLineIdx >= gMeme.lines.length-1)gMeme.selectedLineIdx=0
-    else gMeme.selectedLineIdx++
+    // else gMeme.selectedLineIdx++
 }
 
 function setLinePos(idx,width, height){
     const line =gMeme.lines[idx]
-    console.log(idx);
-    if(idx===0)line.y = 40
+    // console.log(idx);
+    if(idx===0)line.y = 70
     else if(idx===1)line.y = height-40
     else line.y =height/2
     line.x = width/2
@@ -69,9 +104,35 @@ function getMeme(imgId) {
     }
     return img
 }
+function getCurrMeme(){
+    return gMeme
+}
 
 function getMemes() {
     return gMemes
+}
+
+function getDragStatus(){
+    return gMeme.lines[gMeme.selectedLineIdx].isDrag
+}
+
+function getTextPosition(isCurrLine) {
+    // const gMeme = getCurrMeme()
+    // const { width, size: height, x, y } = gMeme.lines[gMeme.selectedLineIdx]
+    // const { selectedLineIdx: lineIdx } = meme
+    const lines = gMeme.lines.map(line=>{
+        const { width, size: height, x, y,idx }=line
+        const pos = {
+        x: x - width / 2,
+        y: y - height,
+        width,
+        height: height * 1.2,
+        idx,
+    }
+    return pos
+    })
+    if(isCurrLine)return lines[gMeme.selectedLineIdx]
+    return lines
 }
 
 function setLineText(txt) {
